57b8a5b6e4069481d065eac1372e7b3fefc2c322
deepakphpweb <dipakrana2760@gmail.com>
2021-12-10 23:23:51 +0530

    Added : Create a simple linked list

--- src/linkedList/Main.java
+++ src/linkedList/Main.java
@@ -1,4 +1,16 @@
 package linkedList;
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome To LinkedList Data Structure");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 1 to create a simple linked list");
+        switch (sc.nextInt()){
+            case 1:
+                Operation.createSimpleLikedList();
+                break;
+            default:
+        }
+    }
 }
--- src/linkedList/Operation.java
+++ src/linkedList/Operation.java
@@ -1,7 +1,19 @@
 package linkedList;
 
 public class Operation {
-    public static void main(String[] args) {
-        System.out.println("Welcome To LinkedList Data Structure");
+    public static void createSimpleLikedList() {
+        Node firstNode = new Node(56);
+        Node secondNode = new Node(30);
+        Node thirdNode = new Node(70);
+        firstNode .next = secondNode;
+        secondNode.next = thirdNode;
+        Node head = firstNode;
+        Node tail = thirdNode;
+
+        Node temp =head;
+        while(temp!=null){
+            System.out.println(temp.data);
+            temp= temp.next;
+        }
     }
 }
